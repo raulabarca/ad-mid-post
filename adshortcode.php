ee0c7cf8050c485e0003d297b04d7db039df59b5
Raul Abarca <raulabarka@gmail.com>
2016-03-06 19:35:04 +0100

    Edit

--- adshortcode.php
+++ adshortcode.php
@@ -7,7 +7,6 @@ Version: 0.0.1
 Author: Raúl Abarca
 Author URI: http://raulabarca.com/
 */
-
 if (!defined('WP_CONTENT_URL'))
       define('WP_CONTENT_URL', get_option('siteurl').'/wp-content');
 if (!defined('WP_CONTENT_DIR'))
@@ -17,44 +16,52 @@ if (!defined('WP_PLUGIN_URL'))
 if (!defined('WP_PLUGIN_DIR'))
       define('WP_PLUGIN_DIR', WP_CONTENT_DIR.'/plugins');
 
-function activate_adshortcode() {
-  add_option('adcode', 'AABBCC');
-}
+add_action( 'admin_menu', 'adshortcode' );
+function adshortcode_menu() {
+    add_options_page( 'Ad Shortcode', 'Ad Shortcode', 'manage_options', 'adshortcode', 'adshortcode_options' );
+  }
 
-function deactive_adshortcode() {
-  delete_option('adcode');
-}
+function ad_mid_content($content) {
+    if( !is_single() )
+      return $content;
+    global $post; $postid = $post->ID;
 
-function admin_init_adshorcode() {
-  register_setting('web_ad_code', 'adcode');
-}
+    global $table_prefix;
+    $dbh = new wpdb( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST );
+    $table = $table_prefix.'options';
+    $query_cantidad = "SELECT option_value FROM $table WHERE option_name = 'wp_middle_cantidad'";
+    $query_adsense = "SELECT option_value FROM $table WHERE option_name = 'wp_middle_adsense'";
+    $res_cantidad = $dbh->get_results( $query_cantidad );
+    $res_adsense = $dbh->get_results( $query_adsense );
 
-function admin_menu_adshortcode() {
-  add_options_page('Ad Shortcode', 'Ad Shortcode', 'manage_options', 'adshortcode', 'options_page_adshortcode');
-}
+    $data_contenido = $res_cantidad[0]->option_value;
+    $word_count = str_word_count(strip_tags($content));
 
-function options_page_adshortcode() {
-  include(WP_PLUGIN_DIR.'/adshortcode/options.php');
-}
+    if($word_count < 190) return $content;
 
-function adshortcode() {
-  $web_ad_code = get_option('web_ad_code');
-  function print_shortcode() {
-    return $web_ad_code;
-  };
-  add_shortcode('ad', 'print_shortcode');
-}
+    $content = explode ( "</p>", $content );
 
-register_activation_hook(__FILE__, 'activate_adshortcode');
-register_deactivation_hook(__FILE__, 'deactive_adshortcode');
+    if($data_contenido == 'medio')
+    {
+      $data_contenido = array(ceil(count($content)/2));
+    }else
+    {
+      $data_contenido = explode(', ', $data_contenido);
+    }
+    $nuevo_contenido = '';
 
-if (is_admin()) {
-  add_action('admin_init', 'admin_init_adshorcode');
-  add_action('admin_menu', 'admin_menu_adshortcode');
-}
+    for ( $i = 0; $i < count ( $content ); $i ++) {
+      if(in_array($i, $data_contenido)){
+        $nuevo_contenido .= '<div class="anuncio_titulo" style="margin: 10px 0px !important;display: block;text-align: center;">';
+        $nuevo_contenido .= stripslashes($res_adsense[0]->option_value);
+        $nuevo_contenido .= '</div>';
+      }
+      $nuevo_contenido .= $content[$i] . "";
+    }
+    return $nuevo_contenido;
+  }
+  add_action('the_content','ad_mid_content' );
 
-if (!is_admin()) {
-  add_action('wp_head', 'adshortcode');
-}
+
 
 ?>
